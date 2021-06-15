1d170ad5d0e8c68fb1a956e356145c6cbff95e14
Anurag Vutukuri <anurag.vturi@gmail.com>
2021-06-15 03:59:19 -0500

    Implemented sorting of columns and formatted date

--- src/Components/EarthquakesList.js
+++ src/Components/EarthquakesList.js
@@ -5,17 +5,11 @@ import '../Styles/styles.css';
 class EarthquakesList extends React.Component{
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {
+            sort: 'asc'
+        };
         this.formatDateTime = this.formatDateTime.bind(this);
-    }
-
-    formatDateTime = (time) =>{
-        let date = new Date(time);
-        let [month, day, year] = [date.toLocaleString('default', { month: 'short' }), date.getDate(), date.getFullYear()];
-        let arr = date.toLocaleTimeString().split(':');
-        let [hour, min, ext] = [arr[0], arr[1], arr[arr.length-1].slice(-2)];
-
-        return `${month} ${day}, ${year}, ${hour}:${min} ${ext}`;
+        this.sortColumn = this.sortColumn.bind(this);
     }
 
     render(){
@@ -29,9 +23,9 @@ class EarthquakesList extends React.Component{
               <table>
                   <thead className='table-header'>
                       <tr>
-                          <td>Title</td>
-                          <td>Magnitude</td>
-                          <td>Time</td>
+                          <td onClick={() => this.sortColumn('place')}>Title</td>
+                          <td onClick={() => this.sortColumn('mag')}>Magnitude</td>
+                          <td onClick={() => this.sortColumn('time')}>Time</td>
                       </tr>
                   </thead>
               </table>
@@ -51,6 +45,45 @@ class EarthquakesList extends React.Component{
                 })}
             </div>
         );
+    }
+
+    formatDateTime = (time) =>{
+        let date = new Date(time);
+        let [month, day, year] = [date.toLocaleString('default', { month: 'short' }), date.getDate(), date.getFullYear()];
+        let arr = date.toLocaleTimeString().split(':');
+        let [hour, min, ext] = [arr[0], arr[1], arr[arr.length-1].slice(-2)];
+
+        return `${month} ${day}, ${year}, ${hour}:${min} ${ext}`;
+    }
+
+    sortColumn = (col) => {
+        this.setState((prevState) =>({
+            sort: prevState.sort === 'asc' ? 'desc' : 'asc'
+        }));
+
+        switch(col){
+            case 'place':{
+                this.state.sort === 'asc' 
+                    ? this.props.data.features.sort((a, b) => a.properties.place.localeCompare(b.properties.place))
+                    : this.props.data.features.sort((a, b) => b.properties.place.localeCompare(a.properties.place))
+                break;
+            }
+            case 'mag':{
+                this.state.sort === 'asc' 
+                    ? this.props.data.features.sort((a, b) => a.properties.mag - b.properties.mag)
+                    : this.props.data.features.sort((a, b) => b.properties.mag - a.properties.mag)
+                break;
+            }
+            case 'time':{
+                this.state.sort === 'asc' 
+                    ? this.props.data.features.sort((a, b) => a.properties.time - b.properties.time)
+                    : this.props.data.features.sort((a, b) => b.properties.time - a.properties.time)
+                break;
+            }
+            default:{
+                break;
+            }
+        }
     }
 }
 
